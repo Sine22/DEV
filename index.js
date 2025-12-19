515a3ba8159382e1e268acfd03099625c97f5dfb
Sai Zayar Tun <151984847+Sine22@users.noreply.github.com>
2025-12-19 11:11:28 +0700

    Refactor index.js for improved structure and clarity

--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const app = express()
 const port = 4444
 
 function Sample(host) {
-  return { name: "Hello", description: "World", url: host }
+	return { name: "Hello", description: "World", url: host }
 }
 
 app.get('/', (req, res) => {
-  res.json(Sample(req.headers.host))
+	res.json(Sample(req.headers.host))
 })
 
-const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+var server = app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
 })
 
 process.on('SIGINT', () => {
-  server.close(() => console.log('Stopped'))
-})
+	console.log('Stopping ...');
+	server.close(() => {
+		console.log('Stopped');
+	});
+});
 
-module.exports = { Sample, server }
+module.exports = {
+	Sample,
+	server
+};
